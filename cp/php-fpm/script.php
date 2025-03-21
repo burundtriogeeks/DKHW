ee256df681427c149e9230df24792810ef1c91eb
burundtriogeeks <burund@triogeeks.com>
2025-03-21 18:17:37 +0200

    my cp

--- cp/php-fpm/script.php
+++ cp/php-fpm/script.php
@@ -6,7 +6,7 @@
         $randomString = '';
 
         for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+            $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
         }
 
         return $randomString;
