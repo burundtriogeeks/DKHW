669e9db1c0215bbf8c27c968f86a45669e2907c0
burundtriogeeks <burund@triogeeks.com>
2025-03-21 18:17:05 +0200

    my cp

--- cp/php-fpm/script.php
+++ cp/php-fpm/script.php
@@ -1,4 +1,17 @@
 <?php
 
-echo "Hello\n";
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
 
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+    }
+
+    do {
+        md5(generateRandomString(64));
+    } while(true);
