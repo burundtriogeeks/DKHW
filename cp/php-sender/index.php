8f1572100c854ca2543f3d220dd5a4dc1a97e143
burundtriogeeks <burund@triogeeks.com>
2025-03-22 16:22:24 +0200

    my cp

--- cp/php-sender/index.php
+++ cp/php-sender/index.php
@@ -1,3 +1,44 @@
 <?php
 
-    echo "test";
+    function curlToRabbit($url,$str,$type = "post") {
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://".getenv("MY_RELEASE_RABBITMQ_SERVICE_HOST").":15672/".$url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        if ($str) {
+            if($type == "post") {
+                curl_setopt($ch, CURLOPT_POST, true);
+            } else {
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $type);
+            }
+            curl_setopt($ch, CURLOPT_POSTFIELDS,$str);
+        }
+        curl_setopt($ch, CURLOPT_USERPWD, "user:".getenv("RABBITMQ_PASSWORD"));
+        $res = curl_exec($ch);
+
+        curl_close($ch);
+        return $res;
+    }
+
+    function createQueue() {
+        $res = curlToRabbit("api/queues/","");
+        if ($res == "[]") {
+            $res = curlToRabbit("api/exchanges/%2f/my.exchange.name",'{"type":"fanout","durable":true}',"PUT");
+            $res = curlToRabbit("api/queues/%2f/my.queue",'{"durable":true,"arguments":{"x-dead-letter-exchange":"", "x-dead-letter-routing-key": "my.queue.dead-letter"}}',"PUT");
+            $res = curlToRabbit("api/bindings/%2f/e/my.exchange.name/q/my.queue",'{"routing_key":"my.queue","arguments":{}}');
+        } else {
+            return;
+        }
+
+    }
+
+    function sendMessage($str) {
+        $res = curlToRabbit("/api/exchanges/%2f/my.exchange.name/publish",'{"properties":{},"routing_key":"my.queue","payload":"'.$str.'","payload_encoding":"string"}');
+        var_dump($res);;
+    }
+
+    createQueue();
+    sendMessage("Some message from client");
+
+    echo "Message sent to queue\n";
